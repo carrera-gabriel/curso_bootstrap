15d25c858d3466ecad651683d1bd6364b083ace3
GabrielCarrera <gabriel.carrera@bandtec.com.br>
2020-07-07 14:54:20 -0300

    Filtro de seleção com jquery e JS

--- js/scripts.js
+++ js/scripts.js
@@ -104,11 +104,48 @@ $(document).ready(function () {
 
     //Parallax
     setTimeout(function () {
-        
+
         $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' });
         $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
 
     }, 250);
+
+    //Filtro do portifólio
+
+    $('.filter-btn').on('click', function () {
+
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes);
+        } else if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes);
+        }
+
+    });
+
+    function eachBoxes(type, boxes) {
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function () {
+                if (!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
+
+    }
 
 });
 
